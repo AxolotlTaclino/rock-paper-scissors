18294b1947c80f91e31cbe25f9acbb122e613340
Cris Allen Taclino <axolotl.taclino@gmail.com>
2022-07-19 17:27:03 +0800

    Added playRound function which takes player input and plays it against the computer generated choice.

--- rps.js
+++ rps.js
@@ -1,9 +1,7 @@
-console.log('test');
+console.log('Rock Paper Scissors Game');
+console.log('------------------------');
 
-let test = getComputerChoice();
-
-console.log(test);
-
+// Generate Computer selection by using a random number between 0-2.
 function getComputerChoice(){
 
     // Generate random number 0 and 2.
@@ -22,3 +20,60 @@ function getComputerChoice(){
     }
     return choice;
 }
+
+// Play one round using User Input vs Computer choice
+function playRound( playerSelection, computerSelection ){
+
+    let roundResult = '';
+
+    // If Player and computer have the same selection, its a tie.
+    if( playerSelection == computerSelection ){
+        roundResult = "It's a tie!"
+    }
+
+    // Else, result will depend on the ff: Rock beats Scissors, Scissors beats Paper, Paper beats Rock.
+    else if( playerSelection == 'rock' ){
+        if( computerSelection == 'scissors' ){
+            roundResult = "You win! Rock beats Scissors."
+        }
+        else{
+            roundResult = "You lose! Paper beats Rock."
+        }
+    }
+    else if( playerSelection == 'paper' ){
+        if( computerSelection == 'rock' ){
+            roundResult = "You win! Paper beats Rock."
+        }
+        else{
+            roundResult = "You lose! Scissors beats Paper."
+        }
+
+    }
+    else if( playerSelection == 'scissors' ){
+        if( computerSelection == 'paper' ){
+            roundResult = "You win! Scissors beats Paper."
+        }
+        else{
+            roundResult = "You lose! Rock beats Scissors."
+        }
+
+    }
+    // Invalid user input.
+    else {
+        roundResult = "Invalid Choice! Please try again."
+    }
+
+    return roundResult;
+}
+// Convert User Input to lowercase to be case insensitive.
+const playerSelection = prompt("Rock Paper or Scissors").toLowerCase();
+
+const computerSelection = getComputerChoice();
+
+console.log("You chose: "+playerSelection); // Show user input
+
+console.log("Computer chose: "+computerSelection); // Show computer generated result
+
+let roundResult = playRound(playerSelection,computerSelection); // Execute single round
+
+console.log(roundResult); // Show Round Result
